7e1e6f55b6efa1b860ccf1adfceb33d64326c834
pgarber <pgarber108@gmail.com>
2013-11-07 16:11:41 -0500

    partway through ch 8

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131105034304) do
+ActiveRecord::Schema.define(version: 20131107191052) do
 
   create_table "users", force: true do |t|
     t.string   "name"
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -22,6 +22,15 @@ describe "UserPages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+    
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+        it { should have_content('error') }
+      end
+
     end
 
     describe "with valid information" do

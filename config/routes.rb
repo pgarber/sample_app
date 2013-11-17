9fcfbfd82af1c4c5ae7376fc0bd5bf2e749f286c
pgarber <pgarber108@gmail.com>
2013-11-17 15:19:37 -0500

    implemented AJAX follow & unfollow

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 SampleApp::Application.routes.draw do
   # get "users/new"  #remove per Listing 7.3
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :sessions, only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
+
   root 'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
   match '/signin',  to: 'sessions#new',         via: 'get'
